969fd83893a7ecc91dd827dfffc2031f65239307
Henry Lau <henry.lau@mirumagency.com>
2019-05-02 17:30:53 +0800

    styled the web site

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -53,6 +53,21 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.render('404',{
+        title:'404',
+        errorMessage:'Help article not found.',
+        name:'Henry Lau'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title:'404',
+        errorMessage:'Page not found.',
+        name:'Henry Lau'
+    })
+})
 
 
 // Starting server
