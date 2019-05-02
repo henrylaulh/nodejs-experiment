345bd47b79803d3c4d30a0457eda71e5ad7d7c50
Henry Lau <henry.lau@mirumagency.com>
2019-05-02 17:01:42 +0800

    added partial views setting and examples. (header / footer)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,43 +1,50 @@
 
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+
+// Setup handlebars engin and views location
 app.set('view engine','hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
 
 
 // Routes
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather App',
-        name: 'Henry'
+        name: 'Henry Lau'
     })
 })
-
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
         image: './img/640x480.png',
-        name: 'Henry'
+        name: 'Henry Lau'
     })
 })
-
 
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help',
         helpText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue neque gravida in fermentum et.',
-        name: 'Henry'
+        name: 'Henry Lau'
     })
 })
-
-
-
-
 
 app.get('/weather', (req, res) => {
     res.send({
