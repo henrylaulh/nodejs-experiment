068886a88c294a6d5c8f62f12db7a0ed56558ac8
Henry Lau <henry.lau@mirumagency.com>
2019-05-02 16:43:59 +0800

    setting up handlebars (hbs) and implementing dynamic data to views

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,49 @@
+
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine','hbs')
+app.use(express.static(publicDirectoryPath))
 
 
 // Routes
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Henry'
+    })
 })
 
-app.get('/help', (req, res) => {
-    res.send('Help page')
-})
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.render('about', {
+        title: 'About Me',
+        image: './img/640x480.png',
+        name: 'Henry'
+    })
 })
 
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue neque gravida in fermentum et.',
+        name: 'Henry'
+    })
+})
+
+
+
+
+
 app.get('/weather', (req, res) => {
-    res.send('View weather')
+    res.send({
+        forecast:'Sunny day',
+        location:'Hong Kong'
+    })
 })
 
 
