f334d701d65614986e2407e279e3db897c0861df
Henry Lau <henry.lau@mirumagency.com>
2019-05-08 18:36:16 +0800

    removing insertion code.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -5,11 +5,6 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id.id)
-console.log(id.id.length)
-console.log(id.toHexString().length)
-// console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     
@@ -18,6 +13,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName)
+
+    // Insertion
 
     // db.collection('users').insertOne({
     //     _id: id,
@@ -30,42 +27,6 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     //     console.log(result.ops)
     // })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Pat',
-    //         age: 32
-    //     },
-    //     {
-    //         name: 'Gunther',
-    //         age: 27
-    //     }
-    // ],(error, result) => {
-    //     if(error){
-    //         return console.log("Unable to insert documents!")
-    //     }
-    //     console.log(result.ops)
-    // })
-
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Eu officia est proident occaecat aliqua minim exercitation ipsum exercitation minim eiusmod fugiat exercitation.',
-    //         completed: true
-    //     },
-    //     {
-    //         description: 'In voluptate cillum labore labore minim aliquip nulla id.',
-    //         completed: false
-    //     },
-    //     {
-    //         description: 'Dolore ex fugiat aliquip esse incididunt proident sunt excepteur id eu adipisicing dolor duis pariatur.',
-    //         completed: true
-    //     },
-    // ], (error, result) => {
-    //     if(error){
-    //         return console.log("Unable to insert tasks!")
-    //     }
-    //     console.log(result.ops)
-    // })
 
 
 
