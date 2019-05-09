779c078e50a0f7f515db7d191b31597ea4bf15e0
Henry Lau <henry.lau@mirumagency.com>
2019-05-09 18:47:15 +0800

    mongoose validations example

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const url = 'mongodb://127.0.0.1:27017/task-manager-api'
 
@@ -9,16 +10,47 @@ mongoose.connect(url, {
 
 // const User = mongoose.model('User', {
 //     name: {
-//         type: String
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     email:{
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value){
+//             if(!validator.isEmail(value)){
+//                 throw new Error('Email is invalid!')
+//             }
+//         }
+//     },
+//     password:{
+//         type: String,
+//         required: true,
+//         trim: true,
+//         minlength: 7,
+//         validate(value){
+//             if(value.toLowerCase().includes("password")){
+//                 throw new Error("Password cannot contain 'password'!")
+//             }
+//         }
 //     },
 //     age: {
-//         type: Number
+//         type: Number,
+//         default: 0,
+//         validate(value) {
+//             if(value < 0){
+//                 throw new Error('Age must be a positive number!')
+//             }
+//         }
 //     }
 // })
 
 // const me = new User({
-//     name: 'Henry Lau',
-//     age: 22
+//     name: '     Henry Lau',
+//     email: 'myemail@henry.lau     ',
+//     password: 'phone098!'
 // })
 
 // me.save().then((reuslt) => {
@@ -28,22 +60,24 @@ mongoose.connect(url, {
 // })
 
 
-const Task = mongoose.model('Task', {
-    description:{
-        type: String
-    },
-    completed:{
-        type: Boolean
-    }
-})
+// const Task = mongoose.model('Task', {
+//     description:{
+//         type: String,
+//         trim: true,
+//         required: true,
+//     },
+//     completed:{
+//         type: Boolean,
+//         default: false,
+//     }
+// })
 
-const cleanHouse = new Task({
-    description: 'Clean the house',
-    completed: false
-})
+// const cleanHouse = new Task({
+//     description: '          Tidy up the room          ',
+// })
 
-cleanHouse.save().then((result) => {
-    console.log(cleanHouse)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// cleanHouse.save().then((result) => {
+//     console.log(cleanHouse)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
