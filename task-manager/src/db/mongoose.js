fe3ea9e57420b002fc5b1172203f573a09b33192
Henry Lau <henrylau9677gmail.com>
2019-05-16 22:41:27 +0800

    Added tasks api call

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 const url = 'mongodb://127.0.0.1:27017/task-manager-api'
 
@@ -7,77 +6,3 @@ mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     email:{
-//         type: String,
-//         required: true,
-//         trim: true,
-//         lowercase: true,
-//         validate(value){
-//             if(!validator.isEmail(value)){
-//                 throw new Error('Email is invalid!')
-//             }
-//         }
-//     },
-//     password:{
-//         type: String,
-//         required: true,
-//         trim: true,
-//         minlength: 7,
-//         validate(value){
-//             if(value.toLowerCase().includes("password")){
-//                 throw new Error("Password cannot contain 'password'!")
-//             }
-//         }
-//     },
-//     age: {
-//         type: Number,
-//         default: 0,
-//         validate(value) {
-//             if(value < 0){
-//                 throw new Error('Age must be a positive number!')
-//             }
-//         }
-//     }
-// })
-
-// const me = new User({
-//     name: '     Henry Lau',
-//     email: 'myemail@henry.lau     ',
-//     password: 'phone098!'
-// })
-
-// me.save().then((reuslt) => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
-
-
-// const Task = mongoose.model('Task', {
-//     description:{
-//         type: String,
-//         trim: true,
-//         required: true,
-//     },
-//     completed:{
-//         type: Boolean,
-//         default: false,
-//     }
-// })
-
-// const cleanHouse = new Task({
-//     description: '          Tidy up the room          ',
-// })
-
-// cleanHouse.save().then((result) => {
-//     console.log(cleanHouse)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,11 +1,39 @@
+
 const express = require('express')
+require('./db/mongoose')
+
+const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.json())
+
 app.post('/users', (req, res) => {
-    res.send('testing!')
+
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+
+    // res.send('testing!')
 })
+
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then((result) => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
 
 app.listen(port, () => {
     console.log("Server is up on port " + port)
