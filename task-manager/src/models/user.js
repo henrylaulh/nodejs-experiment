b89a3651681efd3dd9f885e534855332aed7beff
Henry Lau <henry.lau@mirumagency.com>
2019-05-24 17:37:57 +0800

    Added user authentication logic.

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,7 +2,7 @@
 const validator = require("validator")
 const mongoose = require("mongoose")
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -40,9 +40,34 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number!')
             }
         }
-    }
+    },
+    tokens:[{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
+userSchema.methods.generateAuthToken = async function() {
+    const user = this
+    const token = jwt.sign({_id: user._id.toString()},'thisismynewproject')
+
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+
+    return token
+}
+
+userSchema.methods.getPublicProfile = function() {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 
 router.post('/users', async (req, res) => {
 
@@ -8,7 +9,8 @@ router.post('/users', async (req, res) => {
 
     try{
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     } catch(e){
         res.status(400).send(e)
     }
@@ -17,20 +19,37 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try{
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({user: user.getPublicProfile(), token})
     } catch(e) {
         res.status(400).send(e)
     }
 })
 
-router.get('/users', async (req, res) => {
-
+router.post('/users/logout', auth, async (req, res) => {
     try{
-        const users = await User.find({})
-        res.send(users)
-    }catch (e){
+        req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token)
+
+        await req.user.save()
+
+        res.send()
+    } catch(e) {
         res.status(500).send(e)
     }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try{
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch(e) {
+        res.status(500).send(e)
+    }
+})
+
+router.get('/users/me', auth , async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
