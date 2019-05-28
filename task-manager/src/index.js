1511d9445a221fae856d71452047285a17dcfb1a
Henry Lau <henry.lau@mirumagency.com>
2019-05-28 18:36:01 +0800

    added filtering, sorting, paginating handling to schemas.

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -22,18 +22,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log("Server is up on port " + port)
 })
-
-// const Task = require('./models/task')
-
-const main = async () => {
-    // const task = await Task.findById('5ceb9dc76035c14accf843a5')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('5ceb9ce5534de47e84810eb8')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-
-}
-
-main()
