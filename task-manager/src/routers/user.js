2fc4b688c376dc0cfe8da4a7a7ea77953bc77723
Henry Lau <henry.lau@mirumagency.com>
2019-05-30 11:25:44 +0800

    added user avatar serving API endpoint

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = new express.Router()
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
 const upload = multer({
-    dest:'avatar',
     limits: {
         fileSize: 1000000,
     },
@@ -105,13 +105,37 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+}) 
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
     res.send()
 }, (error, req, res, next) => {
     res.status(400).send({error: error.message})
 })
 
+router.get('/users/:id/avatar', async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id)
+        
+        if(!user || !user.avatar){
+            throw new Error()
+        }
 
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+
+    } catch(e) {
+        res.status(404).send()
+    }
+})
 
 module.exports = router
