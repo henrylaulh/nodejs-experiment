020c7a4e1448795d18836acc55eceb91021dfa3e
Henry Lau <henry.lau@mirumagency.com>
2019-06-06 15:51:05 +0800

    separated app to an individual module for unit test purpose

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,20 +1,5 @@
-
-const express = require('express')
-require('./db/mongoose')
-
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-
-
-
 
 // Start server
 app.listen(port, () => {
